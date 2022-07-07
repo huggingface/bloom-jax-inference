1bde266d2877b1aa432985194ccc752531a0c985
patil-suraj <surajp815@gmail.com>
2022-07-07 10:05:57 +0200

    boom boom

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from  multiprocessing import pool
 import ray
 from ray_tpu import create_tpu, wait_til, get_connection, start_ray
 
-from inference.tpu_cluster import TPUCluster
+from bloom_inference.tpu_manager import TPUManager 
 
 cores_per_replica = 8
 tpu_size = 32
@@ -12,15 +12,14 @@ tpu_size = 32
 tpu_name = "suraj-tpu-v3-32"
 region = "europe-west4-a"
 
-# conns = get_connection(tpu_name, region)
-
+conns = get_connection(tpu_name, region)
 
 head_info = ray.init(include_dashboard=False, object_store_memory=10**9)
-address = head_info['redis_address']
+address = head_info.address_info['address']
 
-# with pool.ThreadPool(processes=len(conns)) as p:
-    # p.map(functools.partial(start_ray, address=address), conns)
+with pool.ThreadPool(processes=len(conns)) as p:
+    p.map(functools.partial(start_ray, address=address), conns)
 
-start_ray(conns, address=address)
+# start_ray(conns, address=address)
 
-t = TPUCluster((tpu_size // cores_per_replica, cores_per_replica), len(conns))
+t = TPUManager((tpu_size // cores_per_replica, cores_per_replica), len(conns))
--- scripts/ray_tpu.sh
+++ scripts/ray_tpu.sh
@@ -6,6 +6,14 @@ set -e
 # this locks the python executable down to hopefully stop if from being fiddled with...
 screen -d -m python -c 'import time; time.sleep(999999999)'
 
+# TODO: this should go in a venv
+# TODO: add checks to see if installation is already done, if yes, skip it.
+
 # initializes jax and installs ray on cloud TPUs
-sudo pip install "jax[tpu]>=0.2.18" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-sudo pip install --upgrade ray[default]==1.5.1 transformers fabric dataclasses optax flax tqdm cloudpickle smart_open[gcs] einops func_timeout
+# sudo pip install --user "jax[tpu]>=0.2.18" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+# sudo pip install --user --upgrade ray==1.13.0 transformers fabric dataclasses optax flax tqdm func_timeout
+
+sudo pip install -e bloom_inference/
+
+# TODO: this should be it's own command.
+TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=34359738368 ray start --address=$1 --resources="{\"tpu\": 1}"
