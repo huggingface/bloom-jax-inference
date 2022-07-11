002c38824a6b263316e9bc5d9b6074fcaf64f7e2
Patrick von Platen <patrick.v.platen@gmail.com>
2022-07-11 01:08:44 +0000

    adapt run_speed file

--- run_speed.py
+++ run_speed.py
@@ -11,28 +11,29 @@ from flax.jax_utils import replicate, unreplicate
 num_devices = jax.local_device_count()
 
 max_length = 256
-model = FlaxBloomForCausalLM.from_pretrained("sanchit-gandhi/bloom-760m-scan", use_scan=True, do_sample=True, max_length=max_length)
+model = FlaxBloomForCausalLM.from_pretrained("sanchit-gandhi/bloom-760m-scan", use_scan=True, do_sample=False, max_length=max_length)
 params = model.params
 tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-350m")
 
-prompt = "hello there this is a longer sentence"
+prompt = 4 * ["hello there this is a longer sentence", "hi"]
 
 rng = jax.random.PRNGKey(0)
 rngs = jax.random.split(rng, num_devices)
 
-def generate(params, inputs, rng):
-    output_ids = model.generate(**inputs, prng_key=rng, params=params).sequences
+def generate(params, inputs):
+    output_ids = model.generate(**inputs, params=params).sequences
     return output_ids
 
 p_generate = jax.pmap(generate, "batch")
 p_params = replicate(model.params)
 
 def run_generate(input_str):
-    inputs = tokenizer([input_str for i in range(num_devices)], return_tensors="jax", padding="max_length", truncation=True, max_length=32)
+    inputs = tokenizer(prompt, return_tensors="jax", padding="max_length", truncation=True, max_length=32)
     p_inputs = shard(inputs.data)
-    output_ids = p_generate(p_params, p_inputs, rngs)
+    # output_ids = p_generate(p_params, p_inputs, rngs)
+    output_ids = p_generate(p_params, p_inputs)
     output_strings = tokenizer.batch_decode(output_ids.reshape(-1, max_length), skip_special_tokens=True)
-    return output_strings
+    return output_strings[:2]
 
 print("generated text", run_generate(prompt))
 
