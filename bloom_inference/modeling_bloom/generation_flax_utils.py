25dab8e03e54e2f91b5308be3e398b7b76df5b05
Patrick von Platen <patrick.v.platen@gmail.com>
2022-07-11 02:42:36 +0200

    up

--- bloom_inference/modeling_bloom/generation_flax_utils.py
+++ bloom_inference/modeling_bloom/generation_flax_utils.py
@@ -429,11 +429,10 @@ class FlaxGenerationMixin:
         pad_token_id = pad_token_id if pad_token_id is not None else self.config.pad_token_id
         eos_token_id = eos_token_id if eos_token_id is not None else self.config.eos_token_id
 
-        batch_size, cur_len = input_ids.shape
+        batch_size, _ = input_ids.shape
 
         eos_token_id = jnp.array(eos_token_id)
         pad_token_id = jnp.array(pad_token_id)
-        cur_len = jnp.array(cur_len)
 
         # per batch-item holding current token in loop.
         sequences = jnp.full((batch_size, max_length), pad_token_id, dtype=jnp.int32)
@@ -448,11 +447,15 @@ class FlaxGenerationMixin:
         # initialize model specific kwargs
         model_kwargs = self.prepare_inputs_for_generation(input_ids, max_length, **model_kwargs)
 
+        # define starting prompt
+        running_token = input_ids[:, :1]
+        cur_len = jnp.array(1)
+
         # initialize state
         state = GreedyState(
             cur_len=cur_len,
             sequences=sequences,
-            running_token=input_ids,
+            running_token=running_token,
             is_sent_finished=is_sent_finished,
             model_kwargs=model_kwargs,
         )
@@ -476,10 +479,15 @@ class FlaxGenerationMixin:
 
             next_token = next_token * ~state.is_sent_finished + pad_token_id * state.is_sent_finished
             next_is_sent_finished = state.is_sent_finished | (next_token == eos_token_id)
-            next_token = next_token[:, None]
 
+            # overwrite token only if token is new
+            next_is_already_generated = state.sequences[:, state.cur_len] != pad_token_id
+            next_token = (1 - next_is_already_generated) * next_token +  next_is_already_generated * state.sequences[:, state.cur_len]
+
+            next_token = next_token[:, None]
             next_sequences = lax.dynamic_update_slice(state.sequences, next_token, (0, state.cur_len))
             next_model_kwargs = self.update_inputs_for_generation(model_outputs, state.model_kwargs)
+
             return GreedyState(
                 cur_len=state.cur_len + 1,
                 sequences=next_sequences,
@@ -487,10 +495,6 @@ class FlaxGenerationMixin:
                 is_sent_finished=next_is_sent_finished,
                 model_kwargs=next_model_kwargs,
             )
-
-        # The very first prompt often has sequence length > 1, so run outside of `lax.while_loop` to comply with TPU
-        if input_ids.shape[1] > 1:
-            state = greedy_search_body_fn(state)
 
         if not trace:
             state = self._run_loop_in_debug(greedy_search_cond_fn, greedy_search_body_fn, state)
@@ -518,11 +522,10 @@ class FlaxGenerationMixin:
         eos_token_id = eos_token_id if eos_token_id is not None else self.config.eos_token_id
         prng_key = prng_key if prng_key is not None else jax.random.PRNGKey(0)
 
-        batch_size, cur_len = input_ids.shape
+        batch_size, _ = input_ids.shape
 
         eos_token_id = jnp.array(eos_token_id)
         pad_token_id = jnp.array(pad_token_id)
-        cur_len = jnp.array(cur_len)
 
         # per batch-item holding current token in loop.
         sequences = jnp.full((batch_size, max_length), pad_token_id, dtype=jnp.int32)
@@ -538,11 +541,15 @@ class FlaxGenerationMixin:
         # initialize model specific kwargs
         model_kwargs = self.prepare_inputs_for_generation(input_ids, max_length, **model_kwargs)
 
+        # define starting prompt
+        running_token = input_ids[:, :1]
+        cur_len = jnp.array(1)
+
         # initialize state
         state = SampleState(
             cur_len=cur_len,
             sequences=sequences,
-            running_token=input_ids,
+            running_token=running_token,
             is_sent_finished=is_sent_finished,
             prng_key=prng_key,
             model_kwargs=model_kwargs,
@@ -571,8 +578,12 @@ class FlaxGenerationMixin:
 
             next_is_sent_finished = state.is_sent_finished | (next_token == eos_token_id)
             next_token = next_token * ~next_is_sent_finished + pad_token_id * next_is_sent_finished
-            next_token = next_token[:, None]
 
+            # overwrite token only if token is new
+            next_is_already_generated = state.sequences[:, state.cur_len] != pad_token_id
+            next_token = (1 - next_is_already_generated) * next_token +  next_is_already_generated * state.sequences[:, state.cur_len]
+
+            next_token = next_token[:, None]
             next_sequences = lax.dynamic_update_slice(state.sequences, next_token, (0, state.cur_len))
             next_model_kwargs = self.update_inputs_for_generation(model_outputs, state.model_kwargs)
 
@@ -584,10 +595,6 @@ class FlaxGenerationMixin:
                 model_kwargs=next_model_kwargs,
                 prng_key=prng_key_next,
             )
-
-        # The very first prompt often has sequence length > 1, so run outside of `lax.while_loop` to comply with TPU
-        if input_ids.shape[1] > 1:
-            state = sample_search_body_fn(state)
 
         if not trace:
             state = self._run_loop_in_debug(sample_search_cond_fn, sample_search_body_fn, state)
