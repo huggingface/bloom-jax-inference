c947c8946af0adac1921a11207716365e9552135
patil-suraj <surajp815@gmail.com>
2022-07-12 04:35:22 +0200

    update run example

--- run_speed.py
+++ run_speed.py
@@ -1,53 +1,113 @@
+import argparse
+from time import time
+
 import numpy as np
 import jax
-import time
+import jax.numpy as jnp
+from jax.experimental import PartitionSpec as P
+
+from t5x.partitioning import PjitPartitioner
+from t5x.train_state import InferenceState
+from t5x.checkpoints import Checkpointer
 
 from bloom_inference.modeling_bloom import FlaxBloomForCausalLM, BloomConfig
 from transformers import AutoTokenizer
 
-from flax.training.common_utils import shard
-from flax.jax_utils import replicate, unreplicate
+# create a parser to get ckpt, path, max_len, input_len
+parser = argparse.ArgumentParser()
+parser.add_argument("--ckpt", type=str, default="huggingface/bloom")
+parser.add_argument("--t5x_path", type=str, default="gs://bloom-jax-us-central2-b/bloom-176B-scan-t5x/checkpoint_0")
+parser.add_argument("--max_len", type=int, default=100)
+parser.add_argument("--input_len", type=int, default=10)
+args = parser.parse_args()
 
-num_devices = jax.local_device_count()
 
-max_length = 256
-model = FlaxBloomForCausalLM.from_pretrained("sanchit-gandhi/bloom-760m-scan", use_scan=True, do_sample=True, max_length=max_length)
-params = model.params
-tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-350m")
+ckpt = args.ckpt
+path = args.t5x_path
+max_len = args.max_len
+input_len = args.input_len
 
-prompt = "hello there this is a longer sentence"
+config = BloomConfig.from_pretrained(ckpt)
+model, params = FlaxBloomForCausalLM.from_pretrained(ckpt, _do_init=False, dtype=jnp.bfloat16, use_scan=True)
+tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-350m", use_fast=False)
 
-rng = jax.random.PRNGKey(0)
-rngs = jax.random.split(rng, num_devices)
 
-def generate(params, inputs, rng):
-    output_ids = model.generate(**inputs, prng_key=rng, params=params).sequences
+# 2D parameter and activation partitioning
+logical_axis_rules_full = [
+    ('batch', 'data'),
+    ('mlp', 'model'),
+    ('heads', 'model'),
+    ('vocab', 'model'),
+    # shard both activations and weight matrices on the remaining available axis
+    ('embed', 'model'),
+    ('embed', 'data'),
+    ('kv', None),
+    ('joined_kv', None),
+    ('relpos_buckets', None),
+    ('abspos_buckets', None),
+    ('length', None),
+    ('layers', None),
+    ('stack', None),
+    ('mlp_activations', None),
+]
+
+
+def init_state():
+    input_shape = (1,1)
+    input_ids = jnp.zeros(input_shape, dtype="i4")
+    attention_mask = jnp.ones_like(input_ids)
+    rng = jax.random.PRNGKey(0)
+    initial_vars = model.module.init(rng, input_ids, attention_mask, return_dict=False)
+    return InferenceState.create(initial_vars)
+
+
+state_shapes = jax.eval_shape(init_state)
+
+num_mp_partitions = 4
+partitioner = PjitPartitioner(num_mp_partitions, logical_axis_rules=logical_axis_rules_full)
+mesh_axes = partitioner.get_mesh_axes(state_shapes)
+params_spec = mesh_axes.params
+
+# Instantiate checkpointer
+checkpointer = Checkpointer(state_shapes, partitioner, path, use_gda=True, restore_dtype=jnp.bfloat16, save_dtype=jnp.bfloat16)
+
+# load state
+loaded_state = checkpointer.restore(path=path)
+
+def generate(params, input_ids, attention_mask):
+    output_ids = model.generate(input_ids, attention_mask=attention_mask, params=params).sequences
     return output_ids
 
-p_generate = jax.pmap(generate, "batch")
-p_params = replicate(model.params)
+p_generate = partitioner.partition(
+    generate,
+    in_axis_resources=(params_spec, P("data"), P("data")),
+    out_axis_resources=P("data")
+)
 
-def run_generate(input_str):
-    inputs = tokenizer([input_str for i in range(num_devices)], return_tensors="jax", padding="max_length", truncation=True, max_length=32)
-    p_inputs = shard(inputs.data)
-    output_ids = p_generate(p_params, p_inputs, rngs)
-    output_strings = tokenizer.batch_decode(output_ids.reshape(-1, max_length), skip_special_tokens=True)
-    return output_strings
+# setup for generation
+tokenizer.padding_side = "left"
+model.config.max_length = max_len
+model.config.num_beams = 1
+model.config.do_sample = True
+model.config.top_p = 0.9
 
-print("generated text", run_generate(prompt))
 
-print("----------------------------")
-start_time = time.time()
-out = run_generate(prompt)
-print(len(out))
-print(time.time() - start_time)
-print("----------------------------")
-start_time = time.time()
-out = run_generate(prompt)
-print(len(out))
-print(time.time() - start_time)
-print("----------------------------")
-start_time = time.time()
-out = run_generate(prompt)
-print(len(out))
-print(time.time() - start_time)
+prompts = ["This is cool "] * 4
+inputs = tokenizer(prompts, return_tensors="jax", padding="max_length", truncation=True, max_length=input_len)
+
+# This will auto-magically run in mesh context
+start = time.time()
+gen_ids = p_generate(loaded_state.params, inputs["input_ids"], inputs["attention_mask"])
+generated_text = tokenizer.batch_decode(gen_ids.local_shards[0].data, skip_special_tokens=False)
+if jax.process_index() == 0:
+    print("Compilation time:", time.time() - start)
+
+
+start = time.time()
+gen_ids = p_generate(loaded_state.params, inputs["input_ids"], inputs["attention_mask"])
+generated_text = tokenizer.batch_decode(gen_ids.local_shards[0].data, skip_special_tokens=False)
+if jax.process_index() == 0:
+    print("Generation time:", time.time() - start)
+
+if jax.process_index() == 0:
+    print(generated_text)
