af8ae350e94aebffb2217720103fda5400dec547
patil-suraj <surajp815@gmail.com>
2022-07-12 17:31:30 +0000

    push

--- run.py
+++ run.py
@@ -7,50 +7,49 @@ from ray_tpu import get_connection, start_ray
 
 from bloom_inference.tpu_manager import TPUManager 
 
-num_mp_partitions = 8
-
-#tpu_name = "suraj-tpu-v3-32"
-# tpu_name = "patrick-tpu-v3-32"
-# region = "europe-west4-a"
 tpu_name="bloom-tpu-v4-64"
 region="us-central2-b"
 
-ckpt = "bigscience/bloom-6b3",
-t5x_path = "gs://bloom-jax-us-central2-b/bloom-176B-scan-t5x/checkpoint_0",
-max_len = 256,
-max_input_len = 64,
-model_parallel_submesh = (1, 2, 4, 1), # for v4-64
+ckpt = "bigscience/bloom"
+t5x_path = "gs://bloom-jax-us-central2-b/bloom-176B-scan-t5x/checkpoint_0"
+max_len = 128
+max_input_len = 64
+model_parallel_submesh = (1, 2, 4, 1) # for v4-64
 
 
-# get Python list of TPU hosts
-conns = get_connection(tpu_name, region)
+def setup():
+    # get Python list of TPU hosts
+    conns = get_connection(tpu_name, region)
+    print(len(conns))
+    address='10.130.0.10:8080'
+    head_info = ray.init(include_dashboard=False, address="auto")
+    # object_store_memory=10**9, 
 
-head_info = ray.init(include_dashboard=False, object_store_memory=10**9)
-address = head_info.address_info['address']
+    # start ray CPU<->TPU on all hosts
+    with pool.ThreadPool(processes=len(conns)) as p:
+        p.map(functools.partial(start_ray, address=address), conns)
 
-# start ray CPU<->TPU on all hosts
-with pool.ThreadPool(processes=len(conns)) as p:
-    p.map(functools.partial(start_ray, address=address), conns)
+def init_manager():
+    # initialise TPU manager
+    t = TPUManager(
+        8,
+        ckpt=ckpt,
+        t5x_path=t5x_path,
+        max_len=max_len,
+        max_input_len=max_input_len,
+        model_parallel_submesh=model_parallel_submesh,
+    )
+    return t
 
-# initialise TPU manager
-t = TPUManager(
-    len(conns),
-    ckpt=ckpt,
-    t5x_path=t5x_path,
-    max_len=max_len,
-    max_input_len=max_input_len,
-    model_parallel_submesh=model_parallel_submesh,
-)
+# # benchmark compile step
+# start = time.time()
+# print(t.generate(4*['Recipe for coconut pasta:']))
+# print(f"Generations completed in {time.time() - start:.06}s")
 
-# benchmark compile step
-start = time.time()
-print(t.generate(4*['Recipe for coconut pasta:']))
-print(f"Generations completed in {time.time() - start:.06}s")
+# # benchmark generate
+# start = time.time()
+# print(t.generate(4*['Recipe for coconut pasta:']))
+# print(f"Generations completed in {time.time() - start:.06}s")
 
-# benchmark generate
-start = time.time()
-print(t.generate(4*['Recipe for coconut pasta:']))
-print(f"Generations completed in {time.time() - start:.06}s")
-
-# shutdown ray rpc
-ray.shutdown()
+# # shutdown ray rpc
+# ray.shutdown()
