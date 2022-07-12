a6581a3fdb61554075f9427a167a45b9f1966c03
patil-suraj <surajp815@gmail.com>
2022-07-12 03:23:11 +0000

    add remote cp

---
+++ scripts/remote_cp.sh
@@ -0,0 +1,10 @@
+INSTANCE=bloom-tpu-v4-64
+ZONE=us-central2-b
+PROJECT=my_project
+
+echo copying $1 to $2
+
+for n in ***REMOVED***1..5***REMOVED***; do
+    gcloud alpha compute tpus tpu-vm scp $1 $INSTANCE:$2 --project=$PROJECT --zone=$ZONE --force-key-file-overwrite --strict-host-key-checking=no --worker=all
+    sleep 1
+done
