4ac3a6a68750d12acc0c46a7fe0d14f7b9fb0462
sanchit-gandhi <sanchit@huggingface.co>
2022-07-14 11:47:02 +0100

    add profiling_example.py

--- profiling_example.py
+++ profiling_example.py
@@ -1,24 +1,31 @@
-import numpy as np
-
 import jax
 import jax.numpy as jnp
-from flax.core.frozen_dict import freeze
 from jax.experimental import PartitionSpec as P
+
 from t5x.partitioning import PjitPartitioner
 from t5x.train_state import InferenceState
+from t5x.checkpoints import Checkpointer
 
-from bloom_inference.modeling_bloom import FlaxBloomForCausalLM, BloomConfig
 from transformers import AutoTokenizer
 
-ckpt = "sanchit-gandhi/bloom-350m-scan-t5x"
+from bloom_inference.modeling_bloom import FlaxBloomForCausalLM, BloomConfig
 
-config = BloomConfig(n_layer=1)
-model, params = FlaxBloomForCausalLM.from_pretrained(ckpt, _do_init=False, dtype=jnp.bfloat16, use_scan=True)
-tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-350m", use_fast=False)
+ckpt = "bigscience/bloom-350m"
+t5x_path = "gs://suraj-bloom-tpu-bucket/bloom-350m-scan-t5x/checkpoint_0"
+
+ckpt = "bigscience/bloom-6b3"
+t5x_path = "gs://suraj-tpu-bucket/bloom-6b3-scan-t5x-v3-8-pretrained/checkpoint_0"
+
+max_input_length = 32
+max_new_tokens = 32
+
+model_parallel_submesh = (2, 2, 1, 2)
+
+prompts = 2 * ['the cat sat on the mat']
 
 # 2D parameter and activation partitioning
 logical_axis_rules_full = [
-    ('batch', 'data'),
+    ('batch', None),
     ('mlp', 'model'),
     ('heads', 'model'),
     ('vocab', 'model'),
@@ -36,64 +43,67 @@ logical_axis_rules_full = [
 ]
 
 
-# TODO: Add this in model init
-def init_fn():
+config = BloomConfig.from_pretrained(ckpt)
+model = FlaxBloomForCausalLM(config, _do_init=False, dtype=jnp.bfloat16, use_scan=True)
+
+
+def init_state():
     input_shape = (1, 1)
     input_ids = jnp.zeros(input_shape, dtype="i4")
     attention_mask = jnp.ones_like(input_ids)
     rng = jax.random.PRNGKey(0)
-    return model.module.init(rng, input_ids, attention_mask, return_dict=False)
+    initial_vars = model.module.init(rng, input_ids, attention_mask, return_dict=False)
+    return InferenceState.create(initial_vars)
 
 
-param_axes = jax.eval_shape(init_fn)["params_axes"] # Axis names metadata
-
-# create InferenceState, since the partitioner expects it.
-state = InferenceState(
-    step=jnp.array(0),
-    params=freeze(model.params_shape_tree),
-    params_axes=freeze(param_axes),
-    flax_mutables=None,
-    flax_mutables_axes=param_axes,
+state_shapes = jax.eval_shape(init_state)
+partitioner = PjitPartitioner(
+    model_parallel_submesh=model_parallel_submesh,
+    logical_axis_rules=logical_axis_rules_full
 )
 
-num_mp_partitions = 8
-partitioner = PjitPartitioner(num_mp_partitions, logical_axis_rules=logical_axis_rules_full)
+params_spec = partitioner.get_mesh_axes(state_shapes).params
+# Instantiate checkpointer
+checkpointer = Checkpointer(
+    state_shapes,
+    partitioner,
+    t5x_path,
+    use_gda=True,
+    restore_dtype=jnp.bfloat16,
+    save_dtype=jnp.bfloat16
+)
 
-mesh_axes = partitioner.get_mesh_axes(state)
-params_spec = mesh_axes.params
+# load state
+loaded_state = checkpointer.restore(path=t5x_path)
+tokenizer = AutoTokenizer.from_pretrained(ckpt)
+tokenizer.padding_side = "left"
 
-shard_params = partitioner.partition(model.to_bf16, (params_spec,), params_spec)
+max_length = max_input_length + max_new_tokens
 
-# This will auto-magically run in mesh context
-params = shard_params(freeze(params))
-
-def generate(params, input_ids, attention_mask):
-    output_ids = model.generate(input_ids, attention_mask=attention_mask, params=params).sequences
+def sample_generate(params, input_ids, attention_mask):
+    output_ids = model.generate(input_ids, attention_mask=attention_mask, params=params, do_sample=True,
+                                     num_beams=1, top_p=0.9, max_length=max_length).sequences
     return output_ids
 
-p_generate = partitioner.partition(
-    generate,
-    in_axis_resources=(params_spec, P("data"), P("data")),
-    out_axis_resources=P("data")
-)
+p_sample_generate = partitioner.partition(
+            sample_generate,
+            in_axis_resources=(params_spec, None, None),
+            out_axis_resources=None,
+        )
 
-tokenizer.padding_side = "left"
-model.config.max_length = 256
-model.config.num_beams = 1
-model.config.do_sample = True
-model.config.pad_token_id = tokenizer.pad_token_id
+inputs = tokenizer(prompts, return_tensors="jax", padding=True, truncation=True, max_length=max_input_length,
+                        pad_to_multiple_of=max_input_length)
 
-prompt = "Reciepe for pasta with coconut:"
-inputs = tokenizer([prompt] * 8, return_tensors="jax", padding="max_length", truncation=True, max_length=64)  # BS = 8
+# This will auto-magically run in mesh context
+gen_ids = p_sample_generate(loaded_state.params, inputs["input_ids"], inputs["attention_mask"])
 
-# compile
-gen_ids = p_generate(freeze(params), inputs["input_ids"], inputs["attention_mask"])
 
 with jax.profiler.trace(log_dir="/home/sanchitgandhi/tensorboard"):
     # generation
-    gen_ids = p_generate(freeze(params), inputs["input_ids"], inputs["attention_mask"])
+    gen_ids = p_sample_generate(loaded_state.params, inputs["input_ids"], inputs["attention_mask"])
     gen_ids.block_until_ready()
 
-generated_text = tokenizer.batch_decode(np.asarray(gen_ids), skip_special_tokens=True)
+
+generated_text = tokenizer.batch_decode(gen_ids, skip_special_tokens=True)
 
 print(generated_text)
