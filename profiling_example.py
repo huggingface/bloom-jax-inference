26db029e8c3467bf71513418c519c57157594d4f
sanchit-gandhi <sanchit@huggingface.co>
2022-07-14 12:46:28 +0100

    up

--- profiling_example.py
+++ profiling_example.py
@@ -12,15 +12,22 @@ from bloom_inference.modeling_bloom import FlaxBloomForCausalLM, BloomConfig
 #ckpt = "bigscience/bloom-350m"
 #t5x_path = "gs://suraj-bloom-tpu-bucket/bloom-350m-scan-t5x/checkpoint_0"
 
-ckpt = "bigscience/bloom-6b3"
+"""ckpt = "bigscience/bloom-6b3"
 t5x_path = "gs://suraj-tpu-bucket/bloom-6b3-scan-t5x-v3-8-pretrained/checkpoint_0"
 
 max_input_length = 32
 max_new_tokens = 32
 
-model_parallel_submesh = (2, 2, 1, 2)
+model_parallel_submesh = (2, 2, 1, 2)"""
 
-prompts = 2 * ['the cat sat on the mat']
+ckpt = "bigscience/bloom"
+t5x_path = "gs://bloom-jax-us-central2-b/bloom-176B-scan-t5x-final/checkpoint_0"
+max_new_tokens = 32
+max_input_length = 32
+model_parallel_submesh = (1, 2, 4, 1)  # for v4-64
+
+
+prompts = 8 * ['the cat sat on the mat']
 
 # 2D parameter and activation partitioning
 logical_axis_rules_full = [
@@ -79,10 +86,24 @@ tokenizer.padding_side = "left"
 
 max_length = max_input_length + max_new_tokens
 
+
+def model_forward(params, input_ids, attention_mask):
+    outputs = model(input_ids, attention_mask=attention_mask, params=params)
+    return outputs
+
+
 def sample_generate(params, input_ids, attention_mask):
     output_ids = model.generate(input_ids, attention_mask=attention_mask, params=params, do_sample=True,
                                      num_beams=1, top_p=0.9, max_length=max_length).sequences
     return output_ids
+
+
+p_forward = partitioner.partition(
+            model_forward,
+            in_axis_resources=(params_spec, None, None),
+            out_axis_resources=None,
+        )
+
 
 p_sample_generate = partitioner.partition(
             sample_generate,
@@ -94,10 +115,18 @@ inputs = tokenizer(prompts, return_tensors="jax", padding=True, truncation=True,
                         pad_to_multiple_of=max_input_length)
 
 # This will auto-magically run in mesh context
+forward_ids = p_forward(loaded_state.params, inputs["input_ids"], inputs["attention_mask"])
+
+# This will auto-magically run in mesh context
 gen_ids = p_sample_generate(loaded_state.params, inputs["input_ids"], inputs["attention_mask"])
 
 
-with jax.profiler.trace(log_dir="/tmp/tensorboard"):
+with jax.profiler.trace(log_dir="/tmp/tensorboard/forward"):
+    # generation
+    forward_ids = p_forward(loaded_state.params, inputs["input_ids"], inputs["attention_mask"])
+    forward_ids.block_until_ready()
+
+with jax.profiler.trace(log_dir="/tmp/tensorboard/generate"):
     # generation
     gen_ids = p_sample_generate(loaded_state.params, inputs["input_ids"], inputs["attention_mask"])
     gen_ids.block_until_ready()
@@ -105,4 +134,4 @@ with jax.profiler.trace(log_dir="/tmp/tensorboard"):
 
 generated_text = tokenizer.batch_decode(gen_ids, skip_special_tokens=True)
 
-print(generated_text)
+print(generated_text[0])
