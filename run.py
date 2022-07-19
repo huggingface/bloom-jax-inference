8f0f2b25f085988b6590f13f7ad835151956ccbf
sanchit-gandhi <sanchit@huggingface.co>
2022-07-19 17:47:42 +0100

    v3-32 changes

--- run.py
+++ run.py
@@ -7,23 +7,21 @@ from ray_tpu import get_connection, start_ray
 
 from bloom_inference.tpu_manager import TPUManager 
 
-tpu_name = "dalle-pod"
-region = "us-east1-d"
-project = "dall-e-mega"
+tpu_name = "patrick-tpu-v3-32"
+region = "europe-west4-a"
 
 ckpt = "bigscience/bloom"
 
 t5x_path = "gs://bloom-jax-us-central2-b/bloom-176B-scan-t5x-final/checkpoint_0"
 
-batch_size = 16
+batch_size = 1
 
 max_new_tokens = 64
 max_input_len = 64
 max_len = max_input_len + max_new_tokens
 
-model_parallel_submesh = (1, 8, 1, 2)  # for v3-256
-
-benchmark = True
+model_parallel_submesh = (4, 4, 1, 2)
+num_mp_partitions = 4
 
 # get Python list of TPU hosts
 conns = get_connection(tpu_name, region)
@@ -43,6 +41,7 @@ t = TPUManager(
     max_len=max_len,
     max_input_len=max_input_len,
     model_parallel_submesh=model_parallel_submesh,
+    num_mp_partitions=4,
 )
 
 # benchmark compile step
@@ -55,16 +54,6 @@ start = time.time()
 print(t.generate(batch_size*['Recipe for coconut pasta:']))
 print(f"Generations completed in {time.time() - start:.06}s")
 
-
-if benchmark:
-    for i in [1, 2, 4, 8, 16, 32, 64]:
-        # benchmark compile step
-        t.generate(i*['Recipe for coconut pasta:'])
-
-        # benchmark generate
-        start = time.time()
-        t.generate(i*['Recipe for coconut pasta:'])
-        print(f"{time.time() - start:.06}s")
 
 # shutdown ray rpc
 ray.shutdown()
